832abc392c897f8069b9a8f48c4e3cbf35dc5742
zalogarciam <gegarciam95@gmail.com>
2023-03-22 23:01:07 -0500

    Added model categoria

--- reservas/gestion/models.py
+++ reservas/gestion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    nombre = models.TextField(null=False)
+    habilitado = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'categorias'
--- reservas/reservas/settings.py
+++ reservas/reservas/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'reservas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reservas',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD' : 'scrappy',
+        'PORT': 5432
     }
 }
 
